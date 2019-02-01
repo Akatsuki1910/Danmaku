0738224d0e037a4780c8e25917a4e8ba0e8580f6
akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2019-02-01 09:43:58 +0900

    Update test2.js

--- konva/test2.js
+++ konva/test2.js
@@ -12,48 +12,81 @@ stage.add(layer);
 window.addEventListener('resize', function() {
     location.reload();
 }, false);
+$('canvas').attr('id', 'can');
+var obj = document.getElementById("can");
+var context = obj.getContext('2d');
 //#################################
-var circle = [];
-var p=120;//object
-var add=0;
-var cou=0;
-var anim = new Konva.Animation(function(frame) {
-    var j=0;
-    for(var i=0;i<360;i+=360/p){
-        var xmove = x+Math.cos(((i+90)*(Math.PI/180)))*add;
-        var ymove = y+Math.sin(((i+90)*(Math.PI/180)))*add;
-        circle[j].setX(xmove);
-        circle[j].setY(ymove);
-        j++;
-        if((xmove<0 || xmove>x*2 || ymove<0 || ymove>y*2)){cou++;}
+
+function player(x,y){
+    circle[0][0].setX(x);
+    circle[0][0].setY(y);
+    circle[0][1].setX(x-3);
+    circle[0][1].setY(y-3);
+}
+
+var move_speed = 2;
+document.addEventListener('keydown', (event) => {
+    var player_key_x = circle[0][0].getX();
+    var player_key_y = circle[0][0].getY();
+    switch(event.keyCode){
+        case 37://left
+            player(--player_key_x,player_key_y);
+            break;
+        case 38://up
+            player(player_key_x,--player_key_y);
+            break;
+        case 39://right
+            player(++player_key_x,player_key_y);
+            break;
+        case 40://down
+            player(player_key_x,++player_key_y);
+            break;
     }
-    var a=0;
-    var b=0;
-    if(cou==p){
-        b=p;
-        des(circle,a,b);
-        addobjCircle(a,b,5,"yellow");
-        add=-5*2;
-        }
-    add+=5*2;
-    cou=0;
+});
+
+var circle = [[],[]];
+var p=2;//object
+var add=0;
+var xmove;
+var anim = new Konva.Animation(function(frame) {
+    xmove = Math.floor(x+Math.cos(((180)*(Math.PI/180)))*add);
+    circle[1][0].setX(xmove);
+    circle[1][1].setX(xmove-3);
+    if(rgb_po(circle,0,5)==1){xmove=-1;}
+    if(xmove<0){
+        add=0;
+    }else{
+        add++;
+    }
 }, layer);
 
-function addobjCircle(min,max,rad,color){
-    for(var i=min;i<max-min;i++){
-        circle[i] = new Konva.Circle({
+function addobjCircle(mas,num,rad,color,x,y){
+    for(var i=mas;i<num+mas;i++){
+        circle[i][0] = new Konva.Circle({
             x: x,
             y: y,
             radius: rad,
             fill: color
         });
+        rad=Math.floor(rad*Math.cos(45*(Math.PI/180)));
+        circle[i][1] = new Konva.Rect({
+            x: x-rad,
+            y: y-rad,
+            width: rad*2,
+            height: rad*2,
+            fill: color
+        });
+        layadd(circle[i][0]);
+        layadd(circle[i][1]);
     }
-    for(i=min;i<max-min;i++){layadd(circle[i]);}
 }
 
-function des(obj,min,max){
-    for(var i=min;i<max-min;i++){
-        if(obj[i]){obj[i].destroy();}
+function des(obj,mas,num){
+    for(var i=mas;i<num+mas;i++){
+        if(obj[i][0]){
+            obj[i][0].destroy();
+            obj[i][1].destroy();
+        }
     }
 }
 
@@ -61,6 +94,52 @@ function layadd(obj){
     layer.add(obj);
 }
 
-addobjCircle(0,p,5,"yellow");
+addobjCircle(0,1,5,"red",x-200,y);
+addobjCircle(1,1,5,"blue",x,y);
 
 anim.start();
+
+function rgb_po(obj,p,r){
+    var point=0;
+    var r_end=Math.floor(r*Math.cos(45*(Math.PI/180)));
+    var obj_x = Math.floor(obj[p][0].getX());
+    var obj_y = Math.floor(obj[p][0].getY());
+    var xpo,ypo,rgb_point;
+    //up
+    for(var i=0;i<r_end;i++){
+        xpo = obj_x-r_end+i;
+        ypo = obj_y-r_end;
+        rgb_point = rgbcheck(xpo,ypo);
+        if(rgb_point==1){point=1;break;}
+    }
+    //end
+    for(var i=0;i<r_end;i++){
+        xpo = obj_x-r_end+i;
+        ypo = obj_y+r_end;
+        rgb_point = rgbcheck(xpo,ypo);
+        if(rgb_point==1){point=1;break;}
+    }
+    //side
+    for(var l=1;l<r_end-1;l++){
+        for(var i=0;i<2;i++){
+            xpo = obj_x-r_end+r_end*2*i;
+            ypo = obj_y-r_end+l;
+            rgb_point = rgbcheck(xpo,ypo);
+            if(rgb_point==1){point=1;break;}
+        }
+    }
+    return point;
+}
+
+var rgbcheck = function(x,y){
+    var imagedata = context.getImageData(x, y, 1, 1);
+    var r = imagedata.data[0];
+    var g = imagedata.data[1];
+    var b = imagedata.data[2];
+    if((r==255 && g==0 && b==0) || (r==0 && g==0 && b==0)){
+        return 0;
+    }else{
+        console.log(r+" "+g+" "+b);
+        return 1;
+    }
+};
