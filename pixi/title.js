17c8b646bcdd88cdf28fa350b175984a85bdffaa
akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2019-03-08 17:18:35 +0900

    Add files via upload

--- pixi/title.js
+++ pixi/title.js
@@ -30,19 +30,3 @@ $(document).on("keyup",(e)=>{
 });
 rendererThree.render(scene, camera);
 renderer.render(stage);
-
-
-$('head').append(
-	'<style type="text/css">#container { display: none; } #fade, #loader { display: block; }</style>'
-);
-
-var pageH;
-$(window).on("load",()=>{ // 全ての読み込み完了後に呼ばれる関数
-	pageH = height;
-
-	$("#fade").css("height", pageH).delay(900).fadeOut(800).queue(()=>{
-        titleserect=true;
-    });
-	$("#loader").delay(600).fadeOut(300);
-	$("#pixiview").css("display", "block");
-});
